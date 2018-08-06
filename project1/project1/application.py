90393878605df1b88e77cd13eb4da75619d58488
MaGo1981 <top.underdog01@gmail.com>
2018-08-06 19:42:27 +0200

    Insert form data into database

--- project1/project1/application.py
+++ project1/project1/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session,render_template, request
+from flask import Flask, session,render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -25,15 +25,36 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
+
+
 @app.route("/register", methods=["GET","POST"])
 def register():
+    # imena unosa 'name', 'email', 'pass' po poljima forms (FLASK - forms)
+    name = request.form.get("name")
+    email = request.form.get("email")
+    passw = request.form.get("passw")
+    if request.method == 'POST':
+	    db.execute("INSERT INTO users(name, email, passw) VALUES (:name, :email, :passw)",
+	                {"name": name, "email": email, "passw": passw})
+	    db.commit()
+
+	    return redirect(url_for('login'))
     return render_template("register.html")
+
+
+
 
 @app.route("/login", methods=["GET","POST"])
 def login():
     return render_template("login.html")
 
+
+
 @app.route("/main", methods=["GET","POST"])
 def main():
-	email=request.form.get("email")
-	return render_template("main.html", email=email)
+	username=request.form.get("username")
+	return render_template("main.html", username=username)
+
+@app.route("/logout", methods=["GET","POST"])
+def logout():
+	return render_template("index.html")
