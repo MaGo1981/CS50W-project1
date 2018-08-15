9bdf21a865075dcf70e1b3abd4c4e84ffe731ba6
MaGo1981 <top.underdog01@gmail.com>
2018-08-15 10:34:59 +0200

    error messages; search form.

--- project1/project1/application.py
+++ project1/project1/application.py
@@ -36,10 +36,14 @@ def register():
 	email = request.form.get("email")
 	passw = request.form.get("passw")
 	if request.method == 'POST':
-		db.execute("INSERT INTO users(name, email, passw) VALUES (:name, :email, :passw)",
-			{"name": name, "email": email, "passw": passw})
-		db.commit()
-		return redirect(url_for('login'))
+		try:
+			db.execute("INSERT INTO users(name, email, passw) VALUES (:name, :email, :passw)",
+				{"name": name, "email": email, "passw": passw})
+			db.commit()
+			return redirect(url_for('login'))
+		except:
+			message = "Your code has crashed!"
+			return render_template("error.html", message=message)
 	return render_template("register.html")
 
 
@@ -49,32 +53,46 @@ def register():
 def login():
 	session["name"] = request.form.get("username")
 	session["logged_in"]=True
-	session["user_id"]= db.execute("SELECT id FROM users WHERE name='user3'").fetchone()
+	session["user_id"]= db.execute("SELECT id FROM users WHERE name = :name",
+                            {"name": session["name"]}).fetchone()  #name='user3'").fetchone()
 	return render_template("login.html", user_id=session["user_id"])
 
 
 
 
 
-@app.route("/main", methods=["GET","POST"])
+@app.route("/main", methods=["GET","POST"]) #/<string:username> ili /<string:search> Dynamic Routes RP2 pg 44
 def main():
-	username=request.form.get("username")
-	return render_template("main.html", username=username)
+	session["username"] = request.form.get("username")
+	# session["username"]=username.capitalize()
+	if request.method == 'POST':
+		try:
+			bookSearch=request.form.get("searchform")
+			books = db.execute("SELECT * FROM books WHERE isbn LIKE %:word%  OR title LIKE %:word% OR author LIKE %:word%",
+								{"word": bookSearch}).fetchall()
+			return render_template("main.html", books=books, username=session["username"])
+		except:
+			message = "Your code has crashed! Go back to route /main without logging in!"
+			return render_template("error.html", message=message)
+	else:
+		books = db.execute("SELECT * FROM books WHERE isbn LIKE '%Hat%' OR title LIKE '%Hat%' OR author LIKE '%Hat%'").fetchall()
+		return render_template("main.html", books=books, username=session["username"])
 
-@app.route("/logout", methods=["GET","POST"])
-def logout():
-	return render_template("index.html")
 
 
 
-@app.route("/books")
+
+@app.route("/books", methods=["GET","POST"])
 def books():
-    """Lists all books."""
-    books = db.execute("SELECT * FROM books").fetchall()
-    return render_template("books.html", books=books)
+	session["username"] = request.form.get("username")
+	"""Lists all books."""
+	books = db.execute("SELECT * FROM books").fetchall()
+	return render_template("books.html", books=books, username=session["username"])
 
 
-@app.route("/books/<int:book_id>")
+
+
+@app.route("/books/<int:book_id>", methods=["GET","POST"])
 def book(book_id):
     """Lists details about a single book."""
 
@@ -84,6 +102,47 @@ def book(book_id):
         return render_template("error.html", message="No such book.")
     return render_template("book.html", book=book)
 
+
+
+
+@app.route("/logout", methods=["GET","POST"])
+def logout():
+	return render_template("index.html")
+
+
+
+#--------------------------------------------------------------------------------------------------------
+
+
+
+'''login1() i register1() vjerojatno ne rade jer su na istoj ruti /home???'''
+
+
+
+
+@app.route("/home", methods=["GET","POST"])
+def login1():
+	name = request.form.get("name2")
+	passw = request.form.get("passw2")
+	db_passw = db.execute("SELECT users.passw FROM users WHERE name = name").fetchone()
+	if request.method == 'POST':
+		if db_passw == passw:
+			return redirect(url_for('login'))
+	return render_template("home.html")
+
+
+
+@app.route("/home", methods=["GET","POST"])
+def register1():
+    name1 = request.form.get("name1")
+    email1 = request.form.get("email1")
+    passw1 = request.form.get("passw1")
+    if request.method == 'POST':
+	    db.execute("INSERT INTO users(name, email, passw) VALUES (:name, :email, :passw)",
+	                {"name": name1, "email": email1, "passw": passw1})
+	    db.commit()
+	    return redirect(url_for('login1'))
+    return render_template("home.html")
 
 
 
@@ -99,29 +158,3 @@ def users():
 	except:
 		session["users"]=["Marko", "Marina"]
 		return render_template("users.html", users=session["users"])
-
-
-
-@app.route("/home", methods=["GET","POST"])
-def register1():
-    # imena unosa 'name', 'email', 'pass' po poljima forms (FLASK - forms)
-    name = request.form.get("name1")
-    email = request.form.get("email1")
-    passw = request.form.get("passw1")
-    if request.method == 'POST':
-	    db.execute("INSERT INTO users(name, email, passw) VALUES (:name, :email, :passw)",
-	                {"name": name, "email": email, "passw": passw})
-	    db.commit()
-
-	    return redirect(url_for('register1'))
-    return render_template("home.html")
-
-@app.route("/home", methods=["GET","POST"])
-def login1():
-	name = request.form.get("name2")
-	passw = request.form.get("passw2")
-	db_passw = db.execute("SELECT users.passw FROM users WHERE name = name").fetchone()
-	if request.method == 'POST':
-		if db_passw == passw:
-			return redirect(url_for('login'))
-	return render_template("home.html")
