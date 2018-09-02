dd403a443f55ad07f8efaac26b5579a3fa0d3925
MaGo1981 <top.underdog01@gmail.com>
2018-09-02 11:59:25 +0200

    remove extra folder

--- project1/create_reviews.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE reviews (
-    id SERIAL PRIMARY KEY,
-    rating INTEGER NOT NULL,
-    review VARCHAR NOT NULL,
-    recommend_to VARCHAR NOT NULL,
-    genre VARCHAR NOT NULL,
-    book_id INTEGER REFERENCES books,
-    user_id INTEGER REFERENCES users
-);
